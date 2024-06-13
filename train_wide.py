8540738ebaed7d7c8f46ef1cc17f1eb27852aa9b
Hank <mkrxp123@gmail.com>
2024-06-13 22:11:01 +0800

    calculate loss by similarity

--- train_wide.py
+++ train_wide.py
@@ -1,4 +1,3 @@
-import os
 import argparse
 import numpy as np
 from pathlib import Path
@@ -7,6 +6,7 @@ import torch
 from torch import optim
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
+from torch.nn import functional as F
 from HC_Net.models.network import HCNet
 from dataset_wide import fetch_dataset
 from model_wide import Transformer
@@ -24,20 +24,23 @@ class Experiment:
         self.grd_backbone = self.HC_Net.grd_efficientnet.to(self.device)
         
         self.transformer = Transformer(args).to(self.device)
-        self.optimizer = optim.AdamW(self.transformer.parameters(), lr=args.lr, weight_decay=args.wdecay, eps=args.epsilon)
+        self.alpha_p = args.alpha_p
+        self.alpha_n = args.alpha_n
+        self.optimizer = optim.Adam(self.transformer.parameters(), lr=args.lr, weight_decay=args.wdecay, eps=args.epsilon)
+        # self.optimizer = optim.AdamW(self.transformer.parameters(), lr=args.lr, weight_decay=args.wdecay, eps=args.epsilon)
         
         self.best = 0
         self.step = 0
         self.writer = SummaryWriter(args.log_dir)
         self.weight_path = Path(args.log_dir).joinpath("weight")
-        # self.weight_path.mkdir(parents=True, exist_ok=True)
-        os.makedirs(self.weight_path, exist_ok=True)
+        self.weight_path.mkdir(parents=True, exist_ok=True)
+        # os.makedirs(self.weight_path, exist_ok=True)
     
     @staticmethod
-    def extract_pn(distance: torch.Tensor):
+    def extract_pn(similarity: torch.Tensor):
         '''
         A batch of data contains #batch ground/satellite images
-        Given distance D[i, j] = G[i] - S[j]
+        Given distance D[i, j] = similarity(G[i], S[j])
         D: [batch, batch]
         G: [batch, dim]
         S: [batch, dim]
@@ -45,23 +48,25 @@ class Experiment:
         Therefore, diagonal elements are positive, while others are negative
         '''
         # Create a mask for diagonal elements
-        diag_mask = torch.eye(distance.shape[0], dtype=torch.bool, device=distance.device)
+        diag_mask = torch.eye(similarity.shape[0], dtype=torch.bool, device=similarity.device)
         # Invert the mask to get non-diagonal elements
         non_diag_mask = ~diag_mask
         # Extract elements using the mask
-        positive = distance[diag_mask]
-        negative = distance[non_diag_mask]
+        # print(diag_mask)
+        positive = similarity[diag_mask]
+        negative = similarity[non_diag_mask]
         return positive, negative
     
     @staticmethod
-    def distance(grd, sat):
+    def similarity(grd, sat):
         '''
         grd: [batch, dim]
         sat: [batch, dim]
         '''
-        diff = grd[:, None, :] - sat[None, :, :]
-        dist = torch.sqrt(torch.sum(diff ** 2, dim=-1))
-        return dist
+        # diff = grd[:, None, :] - sat[None, :, :]
+        # matrix = torch.sqrt(torch.sum(diff ** 2, dim=-1))
+        matrix = F.cosine_similarity(grd[:, None, :], sat[None, :, :], dim=-1)
+        return matrix
     
     def extract_feature(self, grd_img, sat_img):
         _, sat_map = self.sat_backbone.extract_features_multiscale(sat_img)
@@ -78,44 +83,46 @@ class Experiment:
             batch_size = grd_img.size(0)
             
             sat_feature, grd_feature = self.extract_feature(grd_img.to(self.device), sat_img.to(self.device))
-            distance = self.distance(grd_feature, sat_feature)
+            matrix = self.similarity(grd_feature, sat_feature)
             
-            # positive, negative = self.extract_pn(distance)
-            # positive_distance_mean = positive.mean()
-            # negative_distance_mean = negative.mean()
-            # record.append(positive_distance_mean.item())
+            # Distance      The lower the more possible
+            # topn_index = torch.topk(matrix, topn, dim=-1, largest=False).indices
             
-            topn_index = torch.topk(distance, topn, dim=-1, largest=False).indices
+            # Similarity    The higher the more possible
+            topn_index = torch.topk(matrix, topn, dim=-1).indices
             # i-th row (ground image) corresponds to i-th col (satellite image)
-            index = torch.arange(batch_size, device=distance.device).reshape(batch_size, -1)
-            accuracy = (index == topn_index).any(dim=-1)
+            index = torch.arange(batch_size, device=matrix.device).reshape(batch_size, -1)
+            accuracy = (index == topn_index).any(dim=-1).sum() / batch_size
             record.append(accuracy.item())
             
         return np.mean(record)
     
-    def loss_fn(self, positive, negative):
+    def loss_fn(self, matrix):
         '''
-        positive: [batch]
-        negative: [batch*(batch-1)]
+        matrix: [batch, batch]
+        Distance or Similarity
         '''
-        # triplet loss
-        loss = torch.clamp(positive - negative + 1, min=0).mean()
+        positive, negative = self.extract_pn(matrix)
+        p_term = torch.mean(torch.log(torch.exp(1 + -self.alpha_p * (positive - 0.6)))) / self.alpha_p
+        n_term = torch.mean(torch.log(torch.exp(1 + self.alpha_n * (negative - 0)))) / self.alpha_n
+        loss = p_term + n_term
         return loss
     
     def train_epoch(self, train_loader):
-        for data in tqdm(train_loader, desc="Train", leave=False, ncols=140):
+        for data in (pbar := tqdm(train_loader, desc="Train", leave=False, ncols=140)):
             self.step += 1
             grd_img, sat_img, *_ = data
             
             sat_feature, grd_feature = self.extract_feature(grd_img.to(self.device), sat_img.to(self.device))
-            distance = self.distance(grd_feature, sat_feature)
-            positive, negative = self.extract_pn(distance)
+            matrix = self.similarity(grd_feature, sat_feature)
             
             self.optimizer.zero_grad()
-            loss = self.loss_fn(positive, negative)
+            loss = self.loss_fn(matrix)
             loss.backward()
             self.optimizer.step()
             
+            if self.step % 25 == 0:
+                pbar.set_postfix_str(f"loss: {loss.item():.3f}")
             self.writer.add_scalar("Train/loss", loss.item(), self.step)
         # self.scheduler.step()
     
@@ -133,8 +140,8 @@ class Experiment:
                 self.best = accuracy
                 torch.save(self.transformer.state_dict(), self.weight_path.joinpath("best.pt"))
             
-            pbar.set_description_str(f"Acc: {accuracy}, best: {self.best}")
             self.writer.add_scalar("Eval/Accuracy", accuracy, epoch + 1)
+            pbar.set_description_str(f"Acc: {accuracy}, best: {self.best}")
         
 
 if __name__ == "__main__":
@@ -149,6 +156,8 @@ if __name__ == "__main__":
     parser.add_argument('--wdecay',     type=float, default=5e-4)
     parser.add_argument('--epsilon',    type=float, default=1e-7)
     parser.add_argument('--topn',       type=int, default=5)
+    parser.add_argument('--alpha_p',    type=float, default=10)
+    parser.add_argument('--alpha_n',    type=float, default=1)
     parser.add_argument('--ckpt',       type=str, default=root.joinpath("best_checkpoint_same.pth"), help="restore checkpoint")
     parser.add_argument('--dataset',    type=str, default=root.joinpath("HC_Net/Data/VIGOR"), help='dataset')    
     parser.add_argument('--log-dir',    type=str, default="log", help="tensorboard/weight location")
@@ -175,5 +184,5 @@ if __name__ == "__main__":
     # args = fetch_config(args)
     experiment = Experiment(args)
     
-    # experiment.train(args)
+    experiment.train(args)
     
